5d3170a7224930d55d3021ade5630b16a2b3dc8e
Tomann734 <torspiekermann@gmail.com>
2025-09-02 01:28:43 +0200

    refactor(backend): improve exception handling

---
+++ backend/src/main/kotlin/de/torpedomirror/backend/dto/ErrorInformationDto.kt
@@ -0,0 +1,5 @@
+package de.torpedomirror.backend.dto
+
+data class ErrorInformationDto(
+    val message: String?,
+)
--- backend/src/main/kotlin/de/torpedomirror/backend/exception/GlobalExceptionHandler.kt
+++ backend/src/main/kotlin/de/torpedomirror/backend/exception/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package de.torpedomirror.backend.exception
 
+import de.torpedomirror.backend.dto.ErrorInformationDto
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -11,32 +12,36 @@ class GlobalExceptionHandler {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @ExceptionHandler(MirrorUserAlreadyExistsException::class)
-    fun handleMirrorUserAlreadyExists(ex: MirrorUserAlreadyExistsException): ResponseEntity<String> {
-        logger.warn(ex.message)
-        return ResponseEntity(ex.message, HttpStatus.CONFLICT)
+    fun handleMirrorUserAlreadyExists(ex: MirrorUserAlreadyExistsException): ResponseEntity<ErrorInformationDto> {
+        return handleException(ex)
     }
 
     @ExceptionHandler(MirrorUserNotFoundException::class)
-    fun handleMirrorUserNotFound(ex: MirrorUserNotFoundException): ResponseEntity<String> {
-        logger.warn(ex.message)
-        return ResponseEntity(ex.message, HttpStatus.NOT_FOUND)
+    fun handleMirrorUserNotFound(ex: MirrorUserNotFoundException): ResponseEntity<ErrorInformationDto> {
+        return handleException(ex)
     }
 
     @ExceptionHandler(ModuleAlreadyExistsException::class)
-    fun handleModuleAlreadyExists(ex: ModuleAlreadyExistsException): ResponseEntity<String> {
-        logger.warn(ex.message)
-        return ResponseEntity(ex.message, HttpStatus.CONFLICT)
+    fun handleModuleAlreadyExists(ex: ModuleAlreadyExistsException): ResponseEntity<ErrorInformationDto> {
+        return handleException(ex)
     }
 
     @ExceptionHandler(ModuleNotFoundException::class)
-    fun handleModuleNotFound(ex: ModuleNotFoundException): ResponseEntity<String> {
-        logger.warn(ex.message)
-        return ResponseEntity(ex.message, HttpStatus.NOT_FOUND)
+    fun handleModuleNotFound(ex: ModuleNotFoundException): ResponseEntity<ErrorInformationDto> {
+        return handleException(ex)
     }
 
     @ExceptionHandler(FileNotFoundException::class)
-    fun handleFileNotFound(ex: FileNotFoundException): ResponseEntity<String> {
-        logger.warn(ex.message)
-        return ResponseEntity(ex.message, HttpStatus.NOT_FOUND)
+    fun handleFileNotFound(ex: FileNotFoundException): ResponseEntity<ErrorInformationDto> {
+        return handleException(ex)
+    }
+
+    private fun handleException(ex: Exception): ResponseEntity<ErrorInformationDto> {
+        val firstElement = ex.stackTrace.firstOrNull()
+        logger.warn("exception: ${ex.message} in ${firstElement?.className}.${firstElement?.methodName}() line ${firstElement?.lineNumber}")
+        val errorInformation = ErrorInformationDto(
+            message = ex.message
+        )
+        return ResponseEntity(errorInformation, HttpStatus.NOT_FOUND)
     }
 }
